fa10c518d8756caf8b9c68a7671ab6db92b99681
Whilker Henrique dos Santos Silva <whilker_santos@hotmail.com>
2024-08-06 10:38:06 -0300

    .

--- Ros/src/control_pkg/src/Record/Record.cpp
+++ Ros/src/control_pkg/src/Record/Record.cpp
@@ -36,7 +36,7 @@ void Record::sensorDataCallBack(const vida_interfaces::msg::SensorDatas::SharedP
 
 void Record::saveSensors()
 {
-    outputSensors.open("../EncoderDataRecorder.txt", ios_base::out | ios_base::app);
+    outputSensors.open("/home/whilker/Documents/Vida/SensorDatasRecord.txt", ios_base::out | ios_base::app);
     outputSensors << BufferDataRecived.str();
     outputSensors.close();
 }
